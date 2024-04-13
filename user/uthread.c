6549610d6751db8b9b720e567cfe83fbff90a985
hzh <good233@sjtu.edu.cn>
2024-04-13 15:36:54 +0800

    finish uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,8 +10,26 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 
 struct thread {
+  struct thread_context     thread_context; /*the thread's registers*/
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
@@ -60,6 +78,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)t, (uint64)current_thread);
   } else
     next_thread = 0;
 }
@@ -74,6 +93,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  memset((void *)&t->stack, 0, STACK_SIZE);
+  memset((void *)&t->thread_context, 0, sizeof(struct thread_context));
+  t->thread_context.sp = (uint64) ((char *)&t->stack + STACK_SIZE);  /*stack pointer set as stack top*/
+  t->thread_context.ra = (uint64) func;  /*return address set as func*/
 }
 
 void 
